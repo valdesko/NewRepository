0eb7ccf098ee49dbd9c3c59fb5b132591e5d0380
valdesko <valdesko4@gmail.com>
2022-04-23 17:03:39 +0400

    frst

--- Selenides/src/test/java/firsttry.java
+++ Selenides/src/test/java/firsttry.java
@@ -1,4 +1,5 @@
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
@@ -9,28 +10,33 @@ import static com.codeborne.selenide.Condition.text;
 class frst {
 
     @BeforeAll
-    static void initDB(){
-        System.out.println("base loaded");
-    }
-
-    @BeforeEach
-    void openings(){
-        open("https://www.google.com/");
-    }
-
-    @AfterEach
-    void endings(){
-        WebDriverRunner.closeWindow();
+    static void setup(){
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
     }
 
     @Test
-    void selenideSearch(){
-        $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=search]").shouldHave(text("selenide.org"));
+    void demoqaTest1(){
+        String name = "Petr";
+        String mail = "petr@mail.com";
+        String address1 = "street1";
+        String address2 = "streestreet";
+
+        open("/text-box");
+        $("[id=userName]").setValue(name);
+        $("[id=userEmail]").setValue(mail);
+        $("[id=currentAddress]").setValue(address1);
+        $("[id=permanentAddress]").setValue(address2);
+        $("#submit").click();
+
+        $("[id=output]").shouldHave(text(name),text(mail));
+        $("[id=output] [id=currentAddress]").shouldHave(text(address1));
+        $("[id=output]").$("[id=permanentAddress]").shouldHave(text(address2));
 
     }
     @Test
     void kotiki(){
+        open("https://google.com/");
         $("[name=q]").setValue("kotiki").pressEnter();
 
     }
